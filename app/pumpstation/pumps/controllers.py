a76168727ecd0f7214f4a4c52772374d93cdf3eb
Hester Hawk <hawk@castle>
2018-08-01 23:12:10 +0200

    pumps list

--- app/pumpstation/pumps/controllers.py
+++ app/pumpstation/pumps/controllers.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, current_app, render_template, flash, redirect, url_for
+from flask import Blueprint, request, current_app, render_template, flash, redirect, url_for
 from flask_login import current_user
+from flask_paginate import Pagination, get_page_parameter
 from datetime import datetime
 
 from .forms.create_pump import CreatePumpForm
@@ -9,9 +10,18 @@ pumps = Blueprint('pumps', __name__, template_folder="views")
 from app import db
 from app.models.pump import Pump
 
+# For pagination
+PER_PAGE = 10
+
 @pumps.route('/pumps', methods=['GET'])
 def all():    
-    return render_template('all_pumps.html')
+
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    pumps = Pump.query.order_by(Pump.created_date).paginate(page, PER_PAGE, False).items
+
+    pagination = Pagination(per_page=PER_PAGE, page=page, total=Pump.query.count(), record_name='pumps', css_framework='bootstrap4')
+
+    return render_template('all_pumps.html', pumps=pumps, pagination=pagination)
 
 @pumps.route('/pump/create', methods=['GET', 'POST'])
 def create():
